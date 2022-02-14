41f019c26ac54f00d8eb887f306952dac454737a
unknown <digitone@naver.com>
2022-02-14 17:39:53 +0900

    111

---
+++ data.js
@@ -0,0 +1,29 @@
+const data = [
+  {
+    id: 0,
+    category: "강아지",
+    blood: "믹스",
+    title: "믹스 무료분양합니다.",
+    gender: "암",
+    area: "서울",
+    imgUrl: 'https://www.zooseyo.com/dog_sale/photo_free/202202/1644805679_72861400.jpg'
+  },
+  {
+    id: 1,
+    category: "고양이",
+    blood: "렉돌",
+    title: "렉돌 무료분양합니다.",
+    gender: "수",
+    area: "서울",
+    imgUrl: 'https://www.zooseyo.com/dog_sale/photo_free/202202/1644804754_43920200.jpeg'
+  },
+  {
+    id: 2,
+    category: "강아지",
+    blood: "퍼그",
+    title: "퍼그 무료분양합니다.",
+    gender: "수",
+    area: "경기도",
+    imgUrl: 'https://www.zooseyo.com/dog_sale/photo_free/202202/1644803469_75000200.jpeg'
+  },
+]
