0f7ab92a4e5cb21bda696477442544736efe1a33
unknown <digitone@naver.com>
2022-02-15 17:39:47 +0900

    data 추가

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 export const data = [
   {
-    id: 0,
+    id: 1,
     category: "강아지",
     blood: "믹스",
     title: "믹스 무료분양합니다.",
@@ -9,7 +9,7 @@ export const data = [
     imgUrl: 'https://www.zooseyo.com/dog_sale/photo_free/202202/1644805679_72861400.jpg'
   },
   {
-    id: 1,
+    id: 2,
     category: "고양이",
     blood: "렉돌",
     title: "렉돌 무료분양합니다.",
@@ -18,12 +18,84 @@ export const data = [
     imgUrl: 'https://www.zooseyo.com/dog_sale/photo_free/202202/1644804754_43920200.jpeg'
   },
   {
-    id: 2,
+    id: 3,
     category: "강아지",
     blood: "퍼그",
     title: "퍼그 무료분양합니다.",
     gender: "수",
     area: "경기도",
     imgUrl: 'https://www.zooseyo.com/dog_sale/photo_free/202202/1644803469_75000200.jpeg'
+  },
+  {
+    id: 4,
+    category: "강아지",
+    blood: "진돗개",
+    title: "진돗개 무료분양합니다.",
+    gender: "암",
+    area: "제주도",
+    imgUrl: 'https://www.zooseyo.com/dog_sale/photo_free/202202/1644803433_38345400.jpg'
+  },
+  {
+    id: 5,
+    category: "강아지",
+    blood: "요크셔테리어",
+    title: "요크셔테리어 무료분양합니다.",
+    gender: "암",
+    area: "울산시",
+    imgUrl: 'https://www.zooseyo.com/dog_sale/photo_free/202202/1644801837_11099200.jpg'
+  },
+  {
+    id: 6,
+    category: "고양이",
+    blood: "스코티쉬 폴드",
+    title: "스코티쉬 폴드 무료분양합니다.",
+    gender: "암",
+    area: "경기도",
+    imgUrl: 'https://www.zooseyo.com/dog_sale/photo_free/1644802535_34241400.jpg'
+  },
+  {
+    id: 7,
+    category: "강아지",
+    blood: "믹스견",
+    title: "믹스견 무료분양합니다.",
+    gender: "암",
+    area: "서울",
+    imgUrl: 'https://www.zooseyo.com/dog_sale/photo_free/202202/1644801459_74476800.jpeg'
+  },
+  {
+    id: 8,
+    category: "고양이",
+    blood: "한국 고양이",
+    title: "한국 고양이 무료분양합니다.",
+    gender: "수",
+    area: "경기도",
+    imgUrl: 'https://www.zooseyo.com/dog_sale/photo_free/202202/1644796898_09618000.jpg'
+  },
+  {
+    id: 9,
+    category: "고양이",
+    blood: "노르웨이 숲",
+    title: "노르웨이 숲 무료분양합니다.",
+    gender: "수",
+    area: "경기도",
+    imgUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Norwegian_Forest_Cat_face.jpg/300px-Norwegian_Forest_Cat_face.jpg'
+  },
+  {
+    id: 10,
+    category: "토끼",
+    blood: "롭이어",
+    title: "롭이어 무료분양합니다.",
+    gender: "수",
+    area: "인천",
+    imgUrl: 'https://www.zooseyo.com/dog_sale/photo_free/202202/1644753130_88980000.jpg'
+  },
+  {
+    id: 11,
+    category: "햄스터",
+    blood: "정글리안",
+    title: "정글리안 무료분양합니다.",
+    gender: "암수",
+    area: "서울시",
+    imgUrl: 'https://www.zooseyo.com/dog_sale/photo_free/202202/1644305308_50181700.jpg'
   },
 ]
